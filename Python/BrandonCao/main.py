3d84d331f1892283945d4e683747fb7d62feab62
Youth Innovation Lab <youthinnolab@gmail.com>
2018-07-19 20:43:52 -0400

    Moonhack Python Project by Brandon Cao
    Moonhack Python Project by Brandon Cao

---
+++ Python/BrandonCao/main.py
@@ -0,0 +1,18 @@
+from classes import *
+
+targetPlanet = input("Type the name of a planet:")
+
+#print(targetPlanet)
+
+myPlanet = ourSolarSystem.getPlanet(targetPlanet)
+
+if myPlanet is not None:
+  moons = (myPlanet.get_moons())
+  if len(moons) > 0:
+    print(moons)
+  else:
+    print(targetPlanet + " has no moons!")
+    
+else:
+    print(targetPlanet+ " is not a planet!")
+    
